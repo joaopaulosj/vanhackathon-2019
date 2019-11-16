4fd9ea0fca46351aeae1e945a409455b6115be24
Joao Paulo <jpsjanuario@gmail.com>
2019-11-16 12:01:12 -0300

    Added splash screen

--- app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/splash/SplashActivity.kt
@@ -1,17 +1,19 @@
 package br.com.joaopaulosj.vanhackathon2019.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import br.com.joaopaulosj.vanhackathon2019.ui.simplelist.createSimpleListIntent
 
 class SplashActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        if(PreferencesHelper.isLogged)
-        startActivity(createSimpleListIntent())
-
-        finish()
-    }
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		
+		Handler().postDelayed({
+			startActivity(createSimpleListIntent())
+			finish()
+		}, 2000)
+		
+	}
 }
