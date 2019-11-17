f40bdb541bdb11486391883930db8de75e34ce0a
Joao Paulo <jpsjanuario@gmail.com>
2019-11-16 22:22:32 -0300

    Replacing tabs

--- app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/EventsFragment.kt
+++ app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/EventsFragment.kt
@@ -7,13 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.joaopaulosj.vanhackathon2019.R
+import kotlinx.android.synthetic.main.fragment_events.view.*
 
 class EventsFragment : Fragment() {
 	
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
 	                          savedInstanceState: Bundle?): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_events, container, false)
+		val view = inflater.inflate(R.layout.fragment_events, container, false)
+		
+		view.webview.apply {
+			settings.javaScriptEnabled = true
+			loadUrl("https://vanhack.com/platform/#/events")
+		}
+		
+		return view
 	}
 	
 	
--- app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/JobsFragment.kt
+++ app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/JobsFragment.kt
@@ -7,13 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.joaopaulosj.vanhackathon2019.R
+import kotlinx.android.synthetic.main.fragment_jobs.view.*
 
 class JobsFragment : Fragment() {
 	
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
 	                          savedInstanceState: Bundle?): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_jobs, container, false)
+		val view = inflater.inflate(R.layout.fragment_jobs, container, false)
+		
+		view.webview.apply {
+			settings.javaScriptEnabled = true
+			loadUrl("https://vanhack.com/platform/#/jobs")
+		}
+		
+		return view
 	}
 	
 	
