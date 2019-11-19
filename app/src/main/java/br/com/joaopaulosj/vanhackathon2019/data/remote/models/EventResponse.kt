6690b9963d5e17e063fa88de8f0fa7135c4b09fd
Joao Paulo <jpsjanuario@gmail.com>
2019-11-18 21:54:46 -0300

    Changing calendar event

--- app/src/main/java/br/com/joaopaulosj/vanhackathon2019/data/remote/models/EventResponse.kt
+++ app/src/main/java/br/com/joaopaulosj/vanhackathon2019/data/remote/models/EventResponse.kt
@@ -1,59 +1,60 @@
 package br.com.joaopaulosj.vanhackathon2019.data.remote.models
 
 import br.com.joaopaulosj.vanhackathon2019.AppConstants
+import br.com.joaopaulosj.vanhackathon2019.utils.extensions.calendarFromString
 import br.com.joaopaulosj.vanhackathon2019.utils.extensions.dateFromString
 import br.com.joaopaulosj.vanhackathon2019.utils.extensions.displayName
 import java.util.*
 
 class EventsResponse(
-    val result: List<EventResponse> = emptyList()
+		val result: List<EventResponse> = emptyList()
 )
 
 class EventResponse(
-    val id: Int,
-    val name: String,
-    val country: String,
-    val city: String,
-    val state: String,
-    val slug: String,
-    val promoted: Boolean,
-    val startDate: String,
-    val endDate: String,
-    val startApplicationDate: String,
-    val endApplicationDate: String,
-    val isOnline: Boolean,
-    val eventType: Int,
-    val applied: Boolean,
-    val possibleToApply: Boolean,
-    val subtitle: String,
-    val theEvent: String,
-    val theCandidates: String,
-    val flag: String,
-    val cover: String,
-    val thumb: String
+		val id: Int,
+		val name: String,
+		val country: String,
+		val city: String,
+		val state: String,
+		val slug: String,
+		val promoted: Boolean,
+		val startDate: String,
+		val endDate: String,
+		val startApplicationDate: String,
+		val endApplicationDate: String,
+		val isOnline: Boolean,
+		val eventType: Int,
+		val applied: Boolean,
+		val possibleToApply: Boolean,
+		val subtitle: String,
+		val theEvent: String,
+		val theCandidates: String,
+		val flag: String,
+		val cover: String,
+		val thumb: String
 ) {
-
-    fun getFlagResName(): String {
-        return "flag_" + country.replace(" ", "").toLowerCase()
-    }
-
-    fun getStart(): Date? {
-        return startDate.dateFromString(AppConstants.DATE_FORMAT)
-    }
-
-    fun getEnd(): Date? {
-        return endDate.dateFromString(AppConstants.DATE_FORMAT)
-    }
-
-    fun getDeadline(): String {
-        val end = endApplicationDate.dateFromString(AppConstants.DATE_FORMAT)
-        return "<b>Deadline</b>: ${end?.displayName("MM/dd/yyyy")}"
-    }
-
-    fun getDate(): String {
-        val start = startDate.dateFromString(AppConstants.DATE_FORMAT)
-        val end = endDate.dateFromString(AppConstants.DATE_FORMAT)
-        return "${start?.displayName("MMMM d")} - ${end?.displayName("d, yyyy")}".capitalize()
-    }
-
+	
+	fun getFlagResName(): String {
+		return "flag_" + country.replace(" ", "").toLowerCase()
+	}
+	
+	fun getStartCal(): Calendar {
+		return startDate.calendarFromString(AppConstants.DATE_FORMAT) ?: Calendar.getInstance()
+	}
+	
+	fun getEndCal(): Calendar {
+		return endDate.calendarFromString(AppConstants.DATE_FORMAT) ?: Calendar.getInstance()
+	}
+	
+	fun getDeadline(): String {
+		val end = endApplicationDate.dateFromString(AppConstants.DATE_FORMAT)
+		return "<b>Deadline</b>: ${end?.displayName("MM/dd/yyyy")}"
+	}
+	
+	fun getDate(): String {
+		val start = startDate.dateFromString(AppConstants.DATE_FORMAT)
+		val end = endDate.dateFromString(AppConstants.DATE_FORMAT)
+		return "${start?.displayName("MMMM d")} - ${end?.displayName("d, yyyy")}".capitalize()
+	}
+	
 }
--- app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/events/EventsFragment.kt
+++ app/src/main/java/br/com/joaopaulosj/vanhackathon2019/ui/home/events/EventsFragment.kt
@@ -1,7 +1,9 @@
 package br.com.joaopaulosj.vanhackathon2019.ui.home.events
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,86 +11,84 @@ import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import br.com.joaopaulosj.vanhackathon2019.R
 import br.com.joaopaulosj.vanhackathon2019.data.remote.models.EventResponse
-import br.com.joaopaulosj.vanhackathon2019.data.repositories.EventsRepository
 import br.com.joaopaulosj.vanhackathon2019.utils.extensions.addTextWatcherDebounce
 import br.com.joaopaulosj.vanhackathon2019.utils.extensions.notImplementedFeature
 import br.com.joaopaulosj.vanhackathon2019.utils.extensions.setup
-import br.com.joaopaulosj.vanhackathon2019.utils.extensions.singleSubscribe
 import kotlinx.android.synthetic.main.fragment_events.*
-import android.content.Intent
-import android.provider.CalendarContract
 import org.jetbrains.anko.longToast
+import java.util.*
 
 
 class EventsFragment : Fragment(), EventsAdapter.OnItemClickListener, EventsContract.View {
-
-    private val presenter: EventsContract.Presenter by lazy {
-        val presenter = EventsPresenter()
-        presenter.attachView(this)
-        presenter
-    }
-
-    private val adapter by lazy {
-        val adapter = EventsAdapter(activity!!, this)
-        ViewCompat.setNestedScrollingEnabled(eventsRv, false)
-        adapter.setHasStableIds(true)
-        eventsRv.setup(adapter)
-        adapter
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_events, container, false)
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        presenter.loadItems()
-        setupSearch()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.detachView()
-    }
-
-    private fun setupSearch() {
-        eventsEt.addTextWatcherDebounce(200) {
-            presenter.filter(eventsEt.text.toString()).apply {
-                adapter.mList = this
-            }
-        }
-    }
-
-    override fun onItemClicked(item: EventResponse) {
-        activity?.notImplementedFeature()
-    }
-
-    override fun onCalendarClicked(item: EventResponse) {
-        val intent = Intent(Intent.ACTION_EDIT).apply {
-            type = "vnd.android.cursor.item/event"
-            putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, item.getStart())
-            putExtra(CalendarContract.EXTRA_EVENT_END_TIME, item.getEnd())
-            putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
-            putExtra(CalendarContract.Events.TITLE, item.name)
-            putExtra(CalendarContract.Events.DESCRIPTION, item.subtitle)
-            putExtra(CalendarContract.Events.EVENT_LOCATION, "${item.city}, ${item.country}")
-        }
-        activity?.startActivity(intent)
-    }
-
-    override fun displayItems(items: List<EventResponse>) {
-        adapter.mList = items
-    }
-
-    override fun displayError(msg: String?) {
-        activity?.longToast(msg ?: "Error")
-    }
-
-    override fun displayLoading(loading: Boolean) {
-
-    }
+	
+	private val presenter: EventsContract.Presenter by lazy {
+		val presenter = EventsPresenter()
+		presenter.attachView(this)
+		presenter
+	}
+	
+	private val adapter by lazy {
+		val adapter = EventsAdapter(activity!!, this)
+		ViewCompat.setNestedScrollingEnabled(eventsRv, false)
+		adapter.setHasStableIds(true)
+		eventsRv.setup(adapter)
+		adapter
+	}
+	
+	override fun onCreateView(
+			inflater: LayoutInflater, container: ViewGroup?,
+			savedInstanceState: Bundle?
+	): View? {
+		val view = inflater.inflate(R.layout.fragment_events, container, false)
+		return view
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		presenter.loadItems()
+		setupSearch()
+	}
+	
+	override fun onDestroy() {
+		super.onDestroy()
+		presenter.detachView()
+	}
+	
+	private fun setupSearch() {
+		eventsEt.addTextWatcherDebounce(200) {
+			presenter.filter(eventsEt.text.toString()).apply {
+				adapter.mList = this
+			}
+		}
+	}
+	
+	override fun onItemClicked(item: EventResponse) {
+		activity?.notImplementedFeature()
+	}
+	
+	override fun onCalendarClicked(item: EventResponse) {
+		val intent = Intent(Intent.ACTION_EDIT).apply {
+			type = "vnd.android.cursor.item/event"
+			putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, item.getStartCal().timeInMillis)
+			putExtra(CalendarContract.EXTRA_EVENT_END_TIME, item.getEndCal().timeInMillis)
+			putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
+			putExtra(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getTimeZone("PST"))
+			putExtra(CalendarContract.Events.TITLE, item.name)
+			putExtra(CalendarContract.Events.DESCRIPTION, item.subtitle)
+			putExtra(CalendarContract.Events.EVENT_LOCATION, "${item.city}, ${item.country}")
+		}
+		activity?.startActivity(intent)
+	}
+	
+	override fun displayItems(items: List<EventResponse>) {
+		adapter.mList = items
+	}
+	
+	override fun displayError(msg: String?) {
+		activity?.longToast(msg ?: "Error")
+	}
+	
+	override fun displayLoading(loading: Boolean) {
+	
+	}
 }
